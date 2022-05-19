e42d8309b238ff133caed8f088cf0b77bd19cbf9
ecknight <ellyknight@gmail.com>
2022-05-19 13:02:17 -0600

    another plot

--- 05_selectclustern.R
+++ 05_selectclustern.R
@@ -7,13 +7,22 @@ ggplot(mc) +
   geom_point(aes(x=kdecluster, y=r, colour=season)) +
   facet_wrap(~nclust)
 
-trend <- read.csv("Data/LBCUClusterTrends.csv")
-
+trend <- read.csv("Data/LBCUClusterTrends.csv") %>% 
+  rename(knncluster=Region)
+clust <- read.csv("Data/LBCUBBSClusters.csv") %>% 
+  left_join(trend %>% 
+              dplyr::select(knncluster, Trend, Relative_Abundance))
+  
 ggplot(trend) +
   geom_point(aes(x=Region_alt, y=Trend)) +
   geom_errorbar(aes(x=Region_alt, ymin=Trend_Q0.025, ymax=Trend_Q0.975)) +
   geom_hline(aes(yintercept=0)) +
   facet_wrap(~nclust)
+
+ggplot(clust) +
+  geom_point(aes(x=X, y=Y, colour=Trend)) +
+  facet_wrap(~nclust) +
+  scale_colour_gradient2(low="red", high="blue", midpoint=0)
 
 overlap <- data.frame(nclust=c(2:6, 8:9),
                       diff = c(1,0,3,5,9,3,10),
