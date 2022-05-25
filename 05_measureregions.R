339c4f8bbc9155047097a43043bf8fc2d11e0a41
ecknight <ellyknight@gmail.com>
2022-05-25 14:27:22 -0600

    Remove exploration, output raster of 95% KDE for raster multiplication of covs

--- 05_measureregions.R
+++ 05_measureregions.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(sf)
 library(sp)
 library(adehabitatHR)
+library(raster)
 
 #1. Set number of clusters----
 n <- 4
@@ -20,78 +21,9 @@ dat.sp <- dat %>%
 #4. Calculate KDE----
 kd <- kernelUD(dat.sp, grid = 1000, extent=2, h="href", same4all=FALSE)
 
-#5. Calculate area----
-kd.95 <- getverticeshr(kd, percent=95, unin="m", unout="km2") %>% 
-  st_as_sf() %>% 
-  mutate(rad = sqrt(area/3.14)) %>% 
-  separate(id, into=c("season", "kdecluster"), remove=FALSE) %>% 
-  mutate(kdecluster =as.numeric(kdecluster))
-kd.50 <- getverticeshr(kd, percent=50, unin="m", unout="km2") %>% 
-  st_as_sf() %>% 
-  mutate(rad = sqrt(area/3.14)) %>% 
-  separate(id, into=c("season", "kdecluster"), remove=FALSE) %>% 
-  mutate(kdecluster =as.numeric(kdecluster))
-
-mcp.95 <- mcp(dat.sp, percent=95, unin="m", unout="km2") %>% 
-  st_as_sf() %>% 
-  separate(id, into=c("season", "kdecluster"), remove=FALSE) %>% 
-  mutate(kdecluster =as.numeric(kdecluster))
-mcp.75 <- mcp(dat.sp, percent=75, unin="m", unout="km2") %>% 
-  st_as_sf() %>% 
-  separate(id, into=c("season", "kdecluster"), remove=FALSE) %>% 
-  mutate(kdecluster =as.numeric(kdecluster))
-mcp.50 <- mcp(dat.sp, percent=50, unin="m", unout="km2") %>% 
-  st_as_sf() %>% 
-  separate(id, into=c("season", "kdecluster"), remove=FALSE) %>% 
-  mutate(kdecluster =as.numeric(kdecluster))
-
-#6. Get centroids----
-kd.cen <- getverticeshr(kd, percent=0.1, unin="m", unout="km2") %>% 
-  st_as_sf() %>% 
-  st_centroid() %>% 
-  st_coordinates() %>% 
-  cbind(kd.area)
-
-#7. Buffer----
-kd.buff <- kd.cen %>% 
-  data.frame() %>% 
-  st_as_sf(coords=c("X", "Y"), crs=3857) %>% 
-  st_buffer(dist=kd.cen$rad*1000)
-  
-#8. Plot----
-bbs <- read.csv("Data/LBCUBBSClusters.csv") %>% 
-  dplyr::filter(nclust==n) %>% 
-  mutate(season="breed")
-
-plot.kde <- ggplot() +
-  geom_sf(data=kd.95, aes(fill=factor(kdecluster)), alpha=0.4) +
-  geom_sf(data=kd.50, aes(fill=factor(kdecluster)), alpha=0.7) +
-  geom_point(data=dat, aes(x=X, y=Y, colour=factor(kdecluster))) +
-#  geom_point(data=bbs, aes(x=X, y=Y, colour=factor(knncluster))) + 
-  geom_point(data=kd.cen, aes(x=X, y=Y), colour="black", size=2) +
-  facet_wrap(~season) +
-  theme(legend.position="none")
-
-plot.mcp <- ggplot() +
-  geom_sf(data=mcp.95, aes(fill=factor(kdecluster)), alpha=0.4) +
-#  geom_sf(data=mcp.75, aes(fill=factor(kdecluster)), alpha=0.7) +
-    geom_point(data=dat, aes(x=X, y=Y, colour=factor(kdecluster))) +
-  #  geom_point(data=bbs, aes(x=X, y=Y, colour=factor(knncluster))) + 
-  facet_wrap(~season) +
-  theme(legend.position="none")
-
-plot.buff <- ggplot() +
-  geom_point(data=dat, aes(x=X, y=Y, colour=factor(kdecluster))) +
-#  geom_point(data=bbs, aes(x=X, y=Y, colour=factor(knncluster))) + 
-  geom_sf(data=kd.buff, aes(fill=factor(kdecluster)), alpha=0.3) +
-#  geom_point(data=kd.cen, aes(x=X, y=Y), colour="black", size=2) +
-  facet_wrap(~season) +
-  theme(legend.position="none")
-
-grid.arrange(plot.kde, plot.mcp, plot.buff, ncol=3, nrow=1)
-
-#using centroids with a gaussian filter may cause high spatial overlap (but does that matter?)
-  
-#9. Save out the two options for GEE----
-write_sf(kd.area, "Data/LBCURegions_KDE.shp")
-write_sf(kd.buff, "Data/LBCURegions_Buffer.shp")
+#5. Rasterize----
+list <- names(kd)
+for(i in 1:length(list)){
+  kd.r <- raster(kd[[i]])
+  raster::writeRaster(kd.r, paste0("gis/kde_", list[i], ".tif"), overwrite=TRUE)
+}
