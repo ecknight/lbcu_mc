a69eeb9113e0cdc428bc518d1e39191e459ea3fd
ecknight <ellyknight@gmail.com>
2022-06-01 10:55:52 -0600

    Select dominant cluster membership

--- 03_clustermonitoringdata.R
+++ 03_clustermonitoringdata.R
@@ -7,8 +7,6 @@ library(adehabitatHR)
 options(scipen=9999)
 
 #TO DO: FIGURE OUT KNN HYPERPARAMETER####
-#TO DO: FIGURE OUT HOW TO DEAL WITH CLUSTERS THAT CAN'T HAVE MCP RUN ON THEM
-#TO DO: SET UP BOOTSTRAPPING
 
 #1. Import tracking data with training clusters----
 track.raw <- read.csv("Data/LBCUKDEClusters.csv")
@@ -36,56 +34,78 @@ bbs.sf <- routes %>%
   mutate(id=paste(countrynum, statenum, Route, sep="-"),
          type="bbs")
 
-#3. Set up loop through # of clusters---
-clusters <- c(2:9)
+#3. Set # of clusters---
+clusters <- 3
+
+#4. Set up bootstrap loop----
+boot <- max(track.raw$boot)
 
 knn.out <- list()
-knn.area <- data.frame()
-for(j in 1:length(clusters)){
+for(i in 1:boot){
   
-  #4. Wrangle tracking data----
-  track.j <- track.raw %>% 
-    dplyr::filter(nclust==clusters[j],
+  #5. Wrangle tracking data----
+  track.i <- track.raw %>% 
+    dplyr::filter(nclust==clusters,
+                  boot==i,
                   season=="breed")
   
-  #5. Put together with BBS data----
-  bbs.j <- bbs.sf %>% 
-            dplyr::select(id, X, Y, type)
-
+  #6. Put together with BBS data----
+  bbs.i <- bbs.sf %>% 
+    dplyr::select(id, X, Y, type)
+  
   #7. KNN with cluster id as initial membership #----
-  knn.j <- knn(train=track.j[,c("X", "Y")], test=bbs.j[,c("X", "Y")], cl=track.j$kdecluster, k=1, prob=TRUE)
-
+  knn.i <- knn(train=track.i[,c("X", "Y")], test=bbs.i[,c("X", "Y")], cl=track.i$kdecluster, k=1, prob=TRUE)
+  
   #8. Keep knn membership of each BBS route and tracking data point----
-  knn.out[[j]] <- data.frame(knncluster=knn.j,
-                        knnprob=attr(knn.j, "prob"),
-                        nclust=clusters[j]) %>% 
-    cbind(bbs.j)
+  knn.out[[i]] <- data.frame(knncluster=knn.i,
+                             knnprob=attr(knn.i, "prob"),
+                             boot=i) %>% 
+    cbind(bbs.i)
   
-  #9. Calculate MCP area for BBSbayes-----
-  sp.j <- SpatialPointsDataFrame(coords=cbind(bbs.j$X, bbs.j$Y), 
-                                   data=data.frame(ID=knn.j),
-                                   proj4string = CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"))
-  
-  mp.j <- mcp(sp.j[,1], percent=100)
-  
-  knn.area <- data.frame(mp.j) %>% 
-    rename(knncluster=id) %>% 
-    mutate(nclust=clusters[j]) %>% 
-    rbind(knn.area)
-  
-  print(paste0("Finished cluster ", clusters[j], " of ", length(clusters)))
+  print(paste0("Finished bootstrap ", i, " of ", boot))
   
 }
 
-#10. Collapse the results----
+#9. Collapse results----
 knn.all <- rbindlist(knn.out)
 
-#11. Visualize----
-ggplot(knn.all) +
-  geom_point(aes(x=X, y=Y, colour=factor(knncluster))) +
-  geom_point(data=filter(track.raw, season=="breed"), aes(x=X, y=Y, colour=factor(kdecluster)), pch=21, fill="white") +
-  facet_wrap(~nclust)
+#10. Look at variation in cluster membership----
+knn.sum <- knn.all %>% 
+  group_by(id, knncluster) %>% 
+  summarize(n=n()) %>% 
+  ungroup()
+summary(knn.sum$n)
 
-#12. Save out----
-write.csv(knn.all, "Data/LBCUBBSClusters.csv", row.names = FALSE)
+knn.99 <- knn.sum %>% 
+  dplyr::filter(n < 100)
+table(knn.99$id)
+#Only 46 points that have 1 instance of cluster variation
+
+#11. Pick mean dominant cluster ID for each route----
+knn.final <- knn.all %>% 
+  group_by(id, X, Y, type) %>% 
+  summarize(knncluster = round(mean(as.numeric(knncluster)))) %>% 
+  ungroup()
+
+#14. Visualize----
+track.final <- track.raw %>% 
+  dplyr::filter(season=="breed", nclust==3, boot==1)
+
+ggplot(knn.final) +
+  geom_point(aes(x=X, y=Y, colour=factor(knncluster))) +
+  geom_point(data=track.final, aes(x=X, y=Y, colour=factor(kdecluster)), pch=21, fill="white")
+
+#15. Calculate MCP area for BBSbayes-----
+sp <- SpatialPointsDataFrame(coords=cbind(knn.final$X, knn.final$Y), 
+                               data=data.frame(ID=knn.final$knncluster),
+                               proj4string = CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"))
+
+mp <- mcp(sp[,1], percent=100)
+
+knn.area <- data.frame(mp) %>% 
+  rename(knncluster=id)
+
+#15. Save out----
+write.csv(knn.all, "Data/LBCUBBSClustersAll.csv", row.names = FALSE)
 write.csv(knn.area, "Data/area_weight.csv", row.names = FALSE)
+write.csv(knn.final, "Data/LBCUBBSClusters.csv", row.names = FALSE)
