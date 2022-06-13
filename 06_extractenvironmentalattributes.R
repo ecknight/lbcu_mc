cac8d3aae64c23fe7869ed229bb9c76c982a6670
ecknight <ellyknight@gmail.com>
2022-06-13 13:23:38 -0600

    Adding more layers

--- 06_extractenvironmentalattributes.R
+++ 06_extractenvironmentalattributes.R
@@ -40,12 +40,39 @@ r.g <- raster_as_ee(r, bucket="lbcu", assetId, overwrite=TRUE)
 #6. Get global surface water layer----
 gsw.occ<-ee$Image('JRC/GSW1_3/GlobalSurfaceWater')$select('occurrence')
 gsw.chg<-ee$Image('JRC/GSW1_3/GlobalSurfaceWater')$select('change_norm')
+gsw.mths<-ee$Image('JRC/GSW1_3/GlobalSurfaceWater')$select('seasonality')
+gsw.yrs<-ee$Image('JRC/GSW1_3/GlobalSurfaceWater')$select('recurrence')
+
+#7. Get cropland data----
+nass <- 
+  
+  
+  #Proportion grassland
+  
+  #Proportion crop
+  
+  #Proportion developed
+  
+  #Grassland to crop conversion
+  
+  #Grassland to developed (tree/urban) conversion
+  
+#8. Get drought data----
+
+
+#9. Get geopotential height----
+
+#10. Get raven density----
+
 
 #7. Multiply rasters together----
 gsw.occ.r <- r.g$multiply(gsw.occ)
 gsw.chg.r <- r.g$multiply(gsw.chg)
+gsw.mths.r <- r.g$multiply(gsw.mths)
+gsw.yrs.r <- r.g$multiply(gsw.yrs)
 
 #8. Stack them----
+stack <- gsw.occ.r$addBands(gsw.chg.r)$addBands(gsw.mths.r)$addBands(gsw.yrs.r)
 
 #9. Send 95% isopleth to gee----
 kde.i <- kde %>% 
@@ -53,6 +80,10 @@ kde.i <- kde %>%
 kde.g <- sf_as_ee(kde.i)
 
 #10. Take mean of raster pixels within 95% isopleth---
+stack.mn <- stack$reduceRegions(reducer=ee$Reducer$mean(), 
+                                          collection=kde.g,
+                                          scale=30)
+
 gsw.occ.mn <- gsw.occ.r$reduceRegions(reducer=ee$Reducer$mean(), 
                                       collection=kde.g,
                                       scale=30)
@@ -61,48 +92,18 @@ gsw.chg.mn <- gsw.chg.r$reduceRegions(reducer=ee$Reducer$mean(),
                                       scale=30)
 
 #11. Export the values----
-task_vector <- ee_table_to_gcs(collection=gsw.occ.mn,
+task_vector <- ee_table_to_gcs(collection=stack.mn,
                                bucket="lbcu", 
                                fileFormat = "CSV",
                                fileNamePrefix = files$id[i])
 task_vector$start()
+ee_monitoring(task_vector, max_attempts=100)
 ee_gcs_to_local(task = task_vector, dsn=paste0("gis/output/", files$id[i], ".csv"))
 val <- read.csv(paste0("gis/output/", files$id[i], ".csv")) %>% 
-  dplyr::select(-.geo)
+  dplyr::select(-.geo) %>% 
+  rename(water.occur = b1, water.change = b1_1, water.months = b1_2, water.years = b1_3)
 
+#12. Get Raven density----
 
-
-#Proportion grassland
-
-#Proportion crop
-
-#Proportion developed
-
-#Grassland to crop conversion
-
-#Grassland to developed (tree/urban) conversion
-
-#Surface water
-
-#Center pivot
-
-#Drought
-
-#Storm frequency
-
-#Max temp
-
-#Raven density
-
-
-#9. Extract the value ----
-pt <- colMeans(xyFromCell(r, which(r[]>0))) %>% 
-  t() %>% 
-  data.frame() %>% 
-  st_as_sf(coords = c('x','y'), crs = 4326)
-pt.g <- sf_as_ee(pt)
-
-gsw.occ.val <- ee_extract(
-  x = gsw.occ.i,
-  y = pt
-)
+#13. Center pivot----
+#Read in delineated shapefile
