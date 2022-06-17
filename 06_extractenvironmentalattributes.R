295b895a55ae036c992e599fc0be980ac9858315
ecknight <ellyknight@gmail.com>
2022-06-17 10:13:44 -0600

    Change to buffer of 50% isopleth radius

--- 06_extractenvironmentalattributes.R
+++ 06_extractenvironmentalattributes.R
@@ -24,144 +24,128 @@ ee_check()
 # 
 # googleCloudStorageR::gcs_create_bucket("lbcu", projectId = project_id)
 
-#3. Get list of raster KDE files----
-files <- data.frame(filepath = list.files("gis", pattern="*.tif", recursive=TRUE, full.names = TRUE)) %>% 
-  separate(filepath, into=c("gis", "level", "id"), sep="/", remove=FALSE) %>% 
-  mutate(id = str_sub(id, -100, -5)) %>% 
-  dplyr::filter(id!="re")
-
-#4. Read in shapefile of 95% isopleths----
-kde <- read_sf("gis/shp/kde_individual.shp")
+#4. Read in shapefile of HRs and get centroid----
+kde <- read_sf("gis/shp/kde_individual.shp") %>% 
+  mutate(id = paste(kdecluster, season, id, year, cluster, sep="-")) %>% 
+  st_make_valid() %>% 
+  st_centroid()
 
 #5. Get Raven density----
 #set_ebirdst_access_key("dkao2v8gv378", overwrite=TRUE)
 #ebirdst_download("comrav")
 ebd <- load_raster("/Users/ellyknight/Library/Application Support/ebirdst/comrav-ERD2019-STATUS-20200930-21f8da4a", product="abundance_seasonal", resolution="hr")
 
-#5. Set up loop----
-#val.gee <- data.frame()
+#6. Get global surface water layer (1984 to 2021)----
+gsw.occ<-ee$Image('JRC/GSW1_3/GlobalSurfaceWater')$select('occurrence')
+gsw.chg<-ee$Image('JRC/GSW1_3/GlobalSurfaceWater')$select('change_norm')
+gsw.mths<-ee$Image('JRC/GSW1_3/GlobalSurfaceWater')$select('seasonality')
+gsw.yrs<-ee$Image('JRC/GSW1_3/GlobalSurfaceWater')$select('recurrence')
+
+#7. Get drought data----
+tclim.drght <- ee$ImageCollection('IDAHO_EPSCOR/TERRACLIMATE')$filter(ee$Filter$date('1984-01-01', '2021-12-31'))$select('pdsi')$mean()
+
+#8. Get landcover data----
+dw.grass <- ee$ImageCollection("GOOGLE/DYNAMICWORLD/V1")$filter(ee$Filter$date('2015-01-01', '2021-12-31'))$select('grass')$mean()
+dw.wetland <- ee$ImageCollection("GOOGLE/DYNAMICWORLD/V1")$filter(ee$Filter$date('2015-01-01', '2021-12-31'))$select('flooded_vegetation')$mean()
+dw.crop <- ee$ImageCollection("GOOGLE/DYNAMICWORLD/V1")$filter(ee$Filter$date('2015-01-01', '2021-12-31'))$select('crops')$mean()
+dw.built <- ee$ImageCollection("GOOGLE/DYNAMICWORLD/V1")$filter(ee$Filter$date('2015-01-01', '2021-12-31'))$select('built')$mean()
+
+#9. Get landcover change data----
+modis1 <- ee$ImageCollection("MODIS/006/MCD12Q1")$filter(ee$Filter$date('2001-01-01', '2001-12-31'))$select('LC_Type1')$mean()
+modis2 <- ee$ImageCollection("MODIS/006/MCD12Q1")$filter(ee$Filter$date('2020-01-01', '2020-12-31'))$select('LC_Type1')$mean()
+
+modis1.grass <- modis1$remap(c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17), c(0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0))
+modis2.crop <- modis2$remap(c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17), c(0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0))
+modis2.built <- modis2$remap(c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17), c(0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0))
+
+modis.crop <- modis1.grass$subtract(modis2.crop)$remap(c(-1,0,1,2), c(0,0,1,0))
+modis.built <- modis1.grass$subtract(modis2.built)$remap(c(-1,0,1,2), c(0,0,1,0))
+
+#13. Stack them----
+stack <- gsw.occ$addBands(gsw.chg)$addBands(gsw.mths)$addBands(gsw.yrs)$addBands(tclim.drght)$addBands(dw.grass)$addBands(dw.wetland)$addBands(dw.crop)$addBands(dw.built)$addBands(modis.crop)$addBands(modis.built)
+
+#10. Set up loop----
 task.list <- list()
 raven <- data.frame()
-for(i in 48:nrow(files)){
+#i <- 1
+for(i in 1:nrow(kde)){
   
-  #6. Read in raster----
-  r <- raster(files$filepath[i]) %>% 
-    projectRaster(crs=4326)
+  #11. Get centroid and send to gee----
+  kde.i <- kde[i,]
+  data <- sf_as_ee(kde.i)
   
-  #7. Send raster to gee---
-  assetId <- sprintf("%s/%s",ee_get_assethome(), files$id[i])
-  r.g <- raster_as_ee(r, bucket="lbcu", assetId, overwrite=TRUE)
+  #12. Buffer----
+  buffer_points <- function(feature){
+    properties <- c("Date", "ID", "Radius", "Type", "date_millis", "ptID", "ptIDn", "X", "Y", "timestamp", "uniq")
+    new_geometry <- feature$geometry()$buffer(kde.i$rad*1000)
+    ee$Feature(new_geometry)$copyProperties(feature, properties)
+  }
   
-  #8. Get global surface water layer (1984 to 2021)----
-  gsw.occ<-ee$Image('JRC/GSW1_3/GlobalSurfaceWater')$select('occurrence')
-  gsw.chg<-ee$Image('JRC/GSW1_3/GlobalSurfaceWater')$select('change_norm')
-  gsw.mths<-ee$Image('JRC/GSW1_3/GlobalSurfaceWater')$select('seasonality')
-  gsw.yrs<-ee$Image('JRC/GSW1_3/GlobalSurfaceWater')$select('recurrence')
+  data.buff <- data$map(buffer_points)
   
-  #9. Get drought data----
-  tclim.drght <- ee$ImageCollection('IDAHO_EPSCOR/TERRACLIMATE')$filter(ee$Filter$date('1984-01-01', '2021-12-31'))$select('pdsi')$mean()
-  
-  #10. Get landcover data----
-  dw.grass <- ee$ImageCollection("GOOGLE/DYNAMICWORLD/V1")$filter(ee$Filter$date('2015-01-01', '2021-12-31'))$select('grass')$mean()
-  dw.wetland <- ee$ImageCollection("GOOGLE/DYNAMICWORLD/V1")$filter(ee$Filter$date('2015-01-01', '2021-12-31'))$select('flooded_vegetation')$mean()
-  dw.crop <- ee$ImageCollection("GOOGLE/DYNAMICWORLD/V1")$filter(ee$Filter$date('2015-01-01', '2021-12-31'))$select('crops')$mean()
-  dw.built <- ee$ImageCollection("GOOGLE/DYNAMICWORLD/V1")$filter(ee$Filter$date('2015-01-01', '2021-12-31'))$select('built')$mean()
-  
-  #11. Get landcover change data----
-  modis1 <- ee$ImageCollection("MODIS/006/MCD12Q1")$filter(ee$Filter$date('2001-01-01', '2001-12-31'))$select('LC_Type1')$mean()
-  modis2 <- ee$ImageCollection("MODIS/006/MCD12Q1")$filter(ee$Filter$date('2020-01-01', '2020-12-31'))$select('LC_Type1')$mean()
-  
-  modis1.grass <- modis1$remap(c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17), c(0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0))
-  modis2.crop <- modis2$remap(c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17), c(0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0))
-  modis2.built <- modis2$remap(c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17), c(0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0))
-  
-  modis.crop <- modis1.grass$subtract(modis2.crop)$remap(c(-1,0,1,2), c(0,0,1,0))
-  modis.built <- modis1.grass$subtract(modis2.built)$remap(c(-1,0,1,2), c(0,0,1,0))
-  
-  #12. Multiply rasters together----
-  gsw.occ.r <- r.g$multiply(gsw.occ)
-  gsw.chg.r <- r.g$multiply(gsw.chg)
-  gsw.mths.r <- r.g$multiply(gsw.mths)
-  gsw.yrs.r <- r.g$multiply(gsw.yrs)
-  tclim.drght.r <- r.g$multiply(tclim.drght)
-  dw.grass.r <- r.g$multiply(dw.grass)
-  dw.wetland.r <- r.g$multiply(dw.wetland)
-  dw.crop.r <- r.g$multiply(dw.crop)
-  dw.built.r <- r.g$multiply(dw.built)
-  modis.crop.r <- r.g$multiply(modis.crop)
-  modis.built.r <- r.g$multiply(modis.built)
-  
-  #13. Stack them----
-  stack <- gsw.occ.r$addBands(gsw.chg.r)$addBands(gsw.mths.r)$addBands(gsw.yrs.r)$addBands(tclim.drght.r)$addBands(dw.grass.r)$addBands(dw.wetland.r)$addBands(dw.crop.r)$addBands(dw.built.r)$addBands(modis.crop.r)$addBands(modis.built.r)
-  
-  #14. Send 95% isopleth to gee----
-  kde.i <- kde %>% 
-    dplyr::filter(id==str_sub(files$id[i], 5, 100))
-  kde.g <- sf_as_ee(kde.i)
-  
-  #15. Take mean of raster pixels within 95% isopleth---
+  #15. Extract mean values---
   stack.mn <- stack$reduceRegions(reducer=ee$Reducer$mean(), 
-                                  collection=kde.g,
+                                  collection=data.buff,
                                   scale=30)
   
   #16. Export the values----
   task_vector <- ee_table_to_gcs(collection=stack.mn,
                                  bucket="lbcu", 
                                  fileFormat = "CSV",
-                                 fileNamePrefix = files$id[i])
+                                 fileNamePrefix = kde.i$id[i])
   task_vector$start()
   task.list[[i]] <- task_vector
-#  ee_monitoring(task_vector, max_attempts=1000)
-#  ee_gcs_to_local(task = task_vector, dsn=paste0("gis/output/", files$id[i], ".csv"))
+  ee_monitoring(task.list[[i]], max_attempts=1000)
+#  ee_gcs_to_local(task = task_vector, dsn=paste0("gis/output/", kde.i$id[i], ".csv"))
   
   #17. Get Raven density----
   kde.ebd <- kde.i %>% 
-    st_transform(crs(ebd))
+    st_transform(crs(ebd)) %>% 
+#    st_buffer(kde.i$rad*1000) %>% 
+    as_Spatial()
+
+  raven <- data.frame(raven = raster::extract(ebd, kde.ebd, buffer=kde.i$rad*1000, fun=mean),
+                       id=kde$id[i]) %>% 
+  rbind(raven)
   
-  r.ebd <- r %>% 
-    projectRaster(crs=crs(ebd))
-  
-  ebd.i <- ebd %>% 
-    crop(r.ebd) %>% 
-    resample(r.ebd) %>% 
-    mask(r.ebd)
-  
-  ebd.m <- ebd.i*r.ebd
-  raven = data.frame(raven = mean(ebd.m@data@values, na.rm=TRUE),
-                     id=str_sub(files$id[i], 5, 100)) %>% 
-    rbind(raven)
-  
-  print(paste0("Finished individual ", i, " of ", nrow(files)))
+  print(paste0("Finished individual ", i, " of ", nrow(kde)))
 }
 
-#17. Put it together----
-val.gee <- data.frame()
-for(i in 1:length(task.list)){
+save.image("gis/TaskList.R")
+
+#17. Download results----
+for(i in 552:nrow(kde)){
   
-  try(ee_gcs_to_local(task = task.list[[i]], dsn=paste0("gis/output/", files$id[i], ".csv")))
-  
-  val.i <- try(read.csv(paste0("gis/output/", files$id[i], ".csv")))
-  
-  if(class(val.i)=="FILL IN"){
-    val.gee <- val.i %>% 
-      dplyr::select(-.geo) %>% 
-      rename(water.occur = b1,
-             water.change = b1_1,
-             water.months = b1_2,
-             water.years = b1_3,
-             drought = b1_4,
-             grass = b1_5,
-             wetland = b1_6,
-             crop = b1_7,
-             built = b1_8,
-             cropchg = b1_9,
-             builtchg = b1_10) %>% 
-      rbind(val.gee)
-  }
+  try(ee_gcs_to_local(task = task.list[[i]], dsn=paste0("gis/output/", kde$id[i], ".csv")))
 
 }
 
-covs <- val.gee %>% 
-  left_join(raven)
+#18. Read in results----
+#val.gee <- data.frame()
+files <- list.files("gis/output")
 
-write.csv(val.gee, "Data/LBCU_environvars.csv", row.names = FALSE)
+dat <- data.frame()
+for(i in 1:length(files)){
+  dat <- read.csv(paste0("gis/output/", files[i])) %>%
+    dplyr::select(-.geo, -system.index) %>% 
+    mutate(id = str_sub(files[i], -100, -5)) %>% 
+    rbind(dat)
+}
+
+#19. Join to other data and tidy----
+covs <- dat %>% 
+  left_join(raven) %>% 
+  left_join(kde %>% 
+              st_coordinates() %>% 
+              data.frame() %>% 
+              cbind(data.frame(kde)) %>% 
+              dplyr::select(-geometry)) %>% 
+  mutate(change_norm = ifelse(is.na(change_norm), 0, change_norm),
+         occurrence = ifelse(is.na(occurrence), 0, occurrence),
+         recurrence = ifelse(is.na(recurrence), 0, recurrence),
+         seasonality = ifelse(is.na(seasonality), 0, seasonality)) %>% 
+  rename(covrop = remapped,
+         covbuilt = remapped_1)
+
+#20. Save-----
+write.csv(covs, "Data/LBCU_environvars.csv", row.names = FALSE)
