ae5d5fe18ca9f6316d0966fc35c9c0a0f4992588
ecknight <ellyknight@gmail.com>
2022-12-29 12:58:13 -0700

    Add manual clustering

--- 01_clustertrackingdata.R
+++ 01_clustertrackingdata.R
@@ -5,34 +5,10 @@ library(data.table)
 options(scipen=9999)
 
 #1. Import data----
-dat.raw <- read.csv("Data/LBCUMCLocations.csv")
+dat <- read.csv("Data/LBCUMCLocations.csv") %>% 
+  rename(seasoncluster=cluster)
 
-#2. Wrangle data to longest duration winter & stopover location for each individual----
-dat.days <- dat.raw %>% 
-  group_by(id, year, season) %>% 
-  arrange(-days) %>% 
-  mutate(cluster.days = row_number()) %>% 
-  ungroup()
-
-dat.days %>% 
-  group_by(season, cluster.days) %>% 
-  summarize(mean = mean(days), 
-            sd = sd(days),
-            max = max(days),
-            min = min(days))
-
-ggplot(dat.days) +
-  geom_histogram(aes(x=days)) +
-  facet_grid(cluster.days ~ season)
-
-# dat <- dat.days %>% 
-# #  dplyr::filter(season %in% c("breed", "winter")) %>% 
-#   dplyr::filter(cluster.days==1)
-dat <- dat.days
-
-table(dat$season)
-
-#3. Use only birds with known breeding & wintering location---
+#2. Use only birds with known breeding & wintering location---
 #ID birds with breeding and wintering ground locations
 dat.n <- dat  %>% 
   dplyr::filter(season %in% c("breed", "winter")) %>% 
@@ -43,59 +19,72 @@ dat.n <- dat  %>%
   dplyr::filter(n==2) %>% 
   ungroup()
 
-#4. Set # of bootstraps & set up loop----
-boot <- 10
+#3. Set # of bootstraps & set up loop----
+boot <- 100
 
 dat.kde <- list()
 set.seed(1234)
 for(i in 1:boot){
   
-  #5. Pick one point for each season for each individual & make it wide----
-  dat.i <- dat.scale %>% 
+  #4. Pick one point for each season for each individual & make it wide----
+  dat.i <- dat %>% 
     dplyr::filter(id %in% dat.n$id) %>% 
     group_by(id, season) %>% 
     sample_n(1) %>% 
-    dplyr::select(id, season, X, Y, distance) %>% 
-    pivot_wider(id_cols=id, names_from=season, values_from=X:distance) %>%
+    ungroup()
+  
+  #Select columns for clustering
+  dat.j <- dat.i %>% 
+    dplyr::select(id, season, X, Y) %>% 
+    pivot_wider(id_cols=id, names_from=season, values_from=X:Y) %>%
     data.frame()
   
-  #8. KDE with incomplete data clustering----
+  dat.k <- dat.j %>% 
+    dplyr::select(-id)
+  
+  #5. KDE with incomplete data clustering----
   clusters <- c(2:6)
   
   kde.cluster <- list()
   for(j in 1:length(clusters)){
-    
-    #Select columns for clustering
-    dat.j <- dat.i %>% 
-      dplyr::select(X_springmig, Y_springmig, X_winter, Y_winter, distance_winter)
-    kde.j <- ClustImpute(dat.j, clusters[j], nr_iter=100)
-    kde.cluster[[j]] <- data.frame(clusters = kde.j$clusters,
+    kde.j <- ClustImpute(dat.k, clusters[j], nr_iter=100)
+    kde.cluster[[j]] <- data.frame(group = kde.j$clusters,
                                    nclust = clusters[j],
-                                   id = dat.i$id)
-    
+                                   id = dat.j$id)
   }
   
+  #6. Add manual clusters----
+  dat.man <- dat.i  %>% 
+    dplyr::filter(season=="winter") %>% 
+    mutate(group = case_when(X > -10960000 & distance < 100000 ~ 4,
+                             lon < -105 & lon > -108 & distance < 10000 ~ 2,
+                             lon < -108 & lon > -118 ~ 2,
+                             lon < -118 ~ 1,
+                             !is.na(lon) ~ 3)) %>% 
+    dplyr::select(id, group) %>% 
+    unique() %>% 
+    full_join(dat.i) %>% 
+    data.frame() %>% 
+    mutate(nclust="manual")
+  
+  #7. Put together----
   dat.kde[[i]] <- rbindlist(kde.cluster) %>% 
-    pivot_wider(id_cols=id, names_from=nclust, values_from=clusters, names_prefix="kde_") %>% 
+    pivot_wider(id_cols=id, names_from=nclust, values_from=group, names_prefix="kde_") %>% 
     left_join(dat.i) %>% 
-    pivot_longer(names_to="nclust", values_to="kdecluster", cols=kde_2:kde_6, names_prefix="kde_") %>% 
-    mutate(nclust = as.numeric(nclust),
-           boot = i)
+    pivot_longer(names_to="nclust", values_to="group", cols=kde_2:kde_6, names_prefix="kde_") %>% 
+    rbind(dat.man) %>% 
+    mutate(boot = i)
   
 print(paste0("Finished bootstrap ", i, " of ", boot))
   
 }
 
 #8. Wrangle output----
-dat.out <- rbindlist(dat.kde) %>% 
-  pivot_longer(cols=c(X_breed:Y_winter),
-               names_to="season", values_to="value") %>%
-  separate(season, into=c("coord", "season")) %>% 
-  pivot_wider(names_from=coord, values_from=value)
+dat.out <- rbindlist(dat.kde)
 
 #9. Look at variation----
 dat.sum <- dat.out %>% 
-  group_by(id, season, nclust, kdecluster) %>% 
+  group_by(id, season, nclust, group) %>% 
   summarize(n=n()) %>% 
   ungroup()
 
@@ -103,35 +92,23 @@ ggplot(dat.sum) +
   geom_histogram(aes(x=n)) +
   facet_grid(season~nclust)
 
-#10. Get main cluster id----
-dat.mean <- dat.out %>% 
-  group_by(id, season, nclust) %>% 
-  summarize(X = mean(X, na.rm=FALSE),
-            Y = mean(Y, na.rm=FALSE),
-            kdecluster = round(mean(kdecluster))) %>%
-  ungroup()
+#10. Remove nclust with < 5 individuals in a cluster----
+# dat.n <- dat.out %>% 
+#   dplyr::filter(season=="winter") %>% 
+#   group_by(nclust, group) %>% 
+#   summarize(n=n()) %>% 
+#   dplyr::filter(n/100 < 5)
+# 
+# dat.final <- dat.out %>% 
+#   dplyr::filter(!nclust %in% dat.n$nclust)
 
-#11. Look at # of individuals per cluster----
-dat.n <- dat.mean %>% 
-  dplyr::filter(season=="winter") %>% 
-  group_by(nclust, kdecluster) %>% 
-  summarize(n=n()) %>% 
-  dplyr::filter(n < 5)
-
-#12. Remove nclust with < 5 individuals in a cluster----
-dat.final <- dat.out %>% 
-  dplyr::filter(!nclust %in% dat.n$nclust)
-
-#13. Plot----
-ggplot(dat.final %>% dplyr::filter(season=="breed", nclust == 3)) +
-  geom_point(aes(x=X, y=Y, colour=factor(kdecluster)), size=4) +
+#11. Plot----
+ggplot(dat.out) +
+  geom_point(aes(x=X, y=Y, colour=factor(group))) +
   facet_grid(season ~ nclust, scales="free_y") + 
   scale_colour_viridis_d()
 
 ggsave(filename="Figs/KDE_stopovers.jpeg", width=18, height = 10)
 
-#14. Save----
-write.csv(dat.final, "Data/LBCUKDEClusters.csv", row.names=FALSE)
-#dat.out <- read.csv("Data/LBCUKDEClusters.csv")
-
-
+#12. Save----
+write.csv(dat.out, "Data/LBCUKDEClusters.csv", row.names=FALSE)
